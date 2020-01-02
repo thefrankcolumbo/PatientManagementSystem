dbf57d864da4cc587da9137c4bed9e99834328f6
Alex Martin <example@test.com>
2020-01-02 12:45:07 +0000

    Added Title field to person class

--- JavaProject/Soft252AMartin/src/people/Person.java
+++ JavaProject/Soft252AMartin/src/people/Person.java
@@ -7,6 +7,7 @@ import java.time.Period;
 
 public abstract class Person 
 {
+    protected String title;
     protected String givenName;
     protected String surname;
     protected String addressLine1;
