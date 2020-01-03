bc946c44c434d23dbe695f917f5934b325fde17c
Alex Martin <example@test.com>
2020-01-03 21:04:40 +0000

    Secretary class updated. Methods (and Java docs) for createPatient and removePatient.

--- JavaProject/Soft252AMartin/src/people/Secretary.java
+++ JavaProject/Soft252AMartin/src/people/Secretary.java
@@ -1,12 +1,16 @@
 
 package people;
 
+import passwordUserID.MakeUser;
+import passwordUserID.RemoveUser;
+import soft252amartin.EPersonType;
+
 
 public class Secretary extends Person implements ICreateAppointment, ICreateAccount
 {
     public Secretary(String UniqueIdentifier)
     {
-        super(UniqueIdentifier);
+        super(UniqueIdentifier, "Secretary");
         
     }
     private void approvePatientAccount()
@@ -21,13 +25,41 @@ public class Secretary extends Person implements ICreateAppointment, ICreateAcco
     {
         
     }
-    private void createPatient()
+    /**
+     * Method to create a new Patient. Password must be in plain text.
+     * Parameter DOB must be in "dd/MM/YYYY" format.
+     * Parameter Password must be in plain text.
+     * If Patient created successfully then Method will return true.
+     * Else Method will return false.
+     * @param givenName
+     * @param surname
+     * @param addressLine1
+     * @param addressLine2
+     * @param addressLine3
+     * @param addressPostcode
+     * @param DOB
+     * @param password
+     * @return boolean
+     */
+    public String createPatient(String givenName, String surname, String addressLine1, 
+            String addressLine2, String addressLine3, String addressPostcode, 
+            String DOB, String password)
     {
-        
+        return MakeUser.makeAUser(givenName, surname, addressLine1, 
+                addressLine2, addressLine3, addressPostcode, DOB, 
+                EPersonType.Patient, DOB);
     }
-    private void removePatient()
+    /**
+     * Method to remove a current Patient.
+     * If doctor removal is unsuccessful then Method will return False.
+     * If userID for Patient does not exist then Method will return False.
+     * Else Method will return True.
+     * @param userID
+     * @return boolean
+     */
+    public boolean removePatient(String userID)
     {
-        
+        return RemoveUser.removeUser(userID, EPersonType.Patient);
     }
 
     @Override
