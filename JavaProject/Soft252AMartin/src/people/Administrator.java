3c04be7feaed2a4dce76f96fd157d99e98aeeaa1
Alex Martin <example@test.com>
2020-01-03 21:09:58 +0000

    Administrator class updated to include methods (and Java docs) for createAdministrator and removeAdministrator.

--- JavaProject/Soft252AMartin/src/people/Administrator.java
+++ JavaProject/Soft252AMartin/src/people/Administrator.java
@@ -13,7 +13,43 @@ public class Administrator extends Person implements IViewDoctorRatings, ICreate
         super(UniqueIdentifier, "Administrator");
     }
     /**
-     * Method to create a new Doctor. Password must be in plain text.
+     * Method to create a new Administrator.
+     * Parameter DOB must be in "dd/MM/YYYY" format.
+     * Parameter Password must be in plain text.
+     * If Administrator created successfully then Method will return true.
+     * Else Method will return false.
+     * @param givenName
+     * @param surname
+     * @param addressLine1
+     * @param addressLine2
+     * @param addressLine3
+     * @param addressPostcode
+     * @param DOB
+     * @param password
+     * @return boolean
+     */
+    public String createAdministrator(String givenName, String surname, String addressLine1, 
+            String addressLine2, String addressLine3, String addressPostcode, 
+            String DOB, String password)
+    {
+        return MakeUser.makeAUser(givenName, surname, addressLine1, 
+                addressLine2, addressLine3, addressPostcode, DOB, 
+                EPersonType.Administrator, DOB);
+    }
+    /**
+     * Method to remove a current Administrator.
+     * If doctor removal is unsuccessful then Method will return False.
+     * If userID for doctor does not exist then Method will return False.
+     * Else Method will return True.
+     * @param userID
+     * @return boolean
+     */
+    public boolean removeAdministrator(String userID)
+    {
+        return RemoveUser.removeUser(userID, EPersonType.Administrator);
+    }
+    /**
+     * Method to create a new Doctor.
      * Parameter DOB must be in "dd/MM/YYYY" format.
      * Parameter Password must be in plain text.
      * If Doctor created successfully then Method will return true.
@@ -53,7 +89,7 @@ public class Administrator extends Person implements IViewDoctorRatings, ICreate
 //        
 //    }
     /**
-     * Method to create a new Secretary. Password must be in plain text.
+     * Method to create a new Secretary.
      * Parameter DOB must be in "dd/MM/YYYY" format.
      * Parameter Password must be in plain text.
      * If Secretary created successfully then Method will return true.
