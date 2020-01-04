cdccbb1e18fe351d3a095d7dc5b9b2f87f8a3263
Alex Martin <example@test.com>
2020-01-04 22:06:52 +0000

    Class Doctor updated to include method to create a medicine

--- JavaProject/Soft252AMartin/src/people/Doctor.java
+++ JavaProject/Soft252AMartin/src/people/Doctor.java
@@ -1,7 +1,7 @@
-
 package people;
 
 import static changeData.AddDataToPatientFile.addPatientReview;
+import static changeData.AddNewMedicine.addANewMedicine;
 import static viewData.ViewPatientNotes.viewPatientNotes;
 
 public class Doctor extends Person 
@@ -32,9 +32,23 @@ public class Doctor extends Person
     {
     
     }
-    private void createNewMedicine()
+    /**
+     * Method to create a new medicine.
+     * This medicine will be created will a 'in stock value of zero'.
+     * The medicineName can not be the same as any other listed medicine - boolean false will be returned.
+     * The parameter unitDefinition is to describe the units the medicine comes in e.g. tables, ml(millilitres).
+     * The parameter medicineNotes are there for explanitory notes that might be required.
+     * All on these parameters are manditory - else boolean false will be returned.
+     * Boolean True will be return on successful creation.
+     * @param medicineName String
+     * @param unitDefinition String
+     * @param medicineNotes String
+     * @return boolean
+     */
+    public boolean createNewMedicine(String medicineName, String unitDefinition,
+            String medicineNotes)
     {
-        
+        return addANewMedicine(medicineName, unitDefinition,medicineNotes);
     }
     private void askSecretaryToOrderMedicine()
     {
