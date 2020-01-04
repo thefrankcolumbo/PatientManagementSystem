433083d3a0c7646861d42e0c226ac552ea7a19b0
Alex Martin <example@test.com>
2020-01-04 15:17:05 +0000

    Superclass Person updated

--- JavaProject/Soft252AMartin/src/people/Person.java
+++ JavaProject/Soft252AMartin/src/people/Person.java
@@ -1,9 +1,18 @@
 
 package people;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import soft252amartin.EPersonType;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public abstract class Person 
 {
@@ -20,44 +29,160 @@ public abstract class Person
     protected EPersonType personType;
     
     // constructor to get an already existing person
-    protected Person(String uniqueIdentifier) 
+    protected Person(String uniqueIdentifier, String type) 
     {
-        getDataFromFile();
+        this.uniqueIdentifier = uniqueIdentifier;
+        loadPerson(type);
         setAge();
     }
-    // constructor to create a new person
-    protected Person(String givenName, String surname, String addressLine1, 
-            String addressLine2, String addressLine3, String addressPostcode, 
-            String DOB, EPersonType personType) 
+    private void loadPerson (String type)
+    {
+        String data = getDataFromFile(type);
+        loadData(data);
+    }
+    private void loadData(String data)
+    {
+        String[] userData = data.split(",");
+        setTitle(userData[0]);
+        setGivenName(userData[1]);
+        setSurname(userData[2]);
+        setAddressLine1(userData[3]);
+        setAddressLine2(userData[4]);
+        setAddressLine3(userData[5]);
+        setAddressPostcode(userData[6]);
+        setDOB(userData[7]);
+        setPersonType(EPersonType.valueOf(userData[8]));
+    }
+    private String getDataFromFile(String type)
+    {
+        String fileContents = "";
+        String path = "res\\" + type + "\\" + uniqueIdentifier + ".txt";
+        try 
+        {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            fileContents = br.readLine();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            //log file??
+        } 
+        catch (IOException e) 
+        {
+            //log file??
+        } 
+        return fileContents;
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // private setters
+    private void setPersonType(EPersonType personType) 
     {
         this.personType = personType;
-        createUniqueIdentifier();
-        createFile();
-        setAge();
     }
-    private void getDataFromFile()
-    {
-        
-    }
-    private void createUniqueIdentifier()
-    {
-        
-    }
-    private void createFile()
-    {
-        
-    }
-
-    private void setPersonType(EPersonType personType) {
-        this.personType = personType;
-    }
-    
     private void setAge()
     {
-        LocalDate dob = LocalDate.parse(this.DOB);
-        LocalDate curDate = LocalDate.now();
-        this.age = Period.between(dob, curDate).getYears();
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+            Date dateOfBirth = sdf.parse(this.DOB);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(dateOfBirth);
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH) + 1;
+            int date = calendar.get(Calendar.DATE);
+            LocalDate l1 = LocalDate.of(year, month, date);
+            LocalDate now1 = LocalDate.now();
+            Period diff1 = Period.between(l1, now1);
+            this.age = (int)diff1.getYears();
+        } 
+        catch (ParseException ex) 
+        {
+            // log ???
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // protected setters
+    protected void setTitle(String title) 
+    {
+        this.title = title;
+    }
+    protected void setGivenName(String givenName) 
+    {
+        this.givenName = givenName;
+    }
+    protected void setSurname(String surname) 
+    {
+        this.surname = surname;
+    }
+    protected void setAddressLine1(String addressLine1) 
+    {
+        this.addressLine1 = addressLine1;
+    }
+    protected void setAddressLine2(String addressLine2) 
+    {
+        this.addressLine2 = addressLine2;
+    }
+    protected void setAddressLine3(String addressLine3) 
+    {
+        this.addressLine3 = addressLine3;
+    }
+    protected void setAddressPostcode(String addressPostcode) 
+    {
+        this.addressPostcode = addressPostcode;
+    }
+    protected void setDOB(String DOB) 
+    {
+        this.DOB = DOB;
+    }
+    private void setUniqueIdentifier(String uniqueIdentifier) 
+    {
+        this.uniqueIdentifier = uniqueIdentifier;
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // public getters
+    public String getTitle() 
+    {
+        return title;
+    }
+    public String getGivenName() 
+    {
+        return givenName;
+    }
+    public String getSurname() 
+    {
+        return surname;
+    }
+    public String getAddressLine1() 
+    {
+        return addressLine1;
+    }
+    public String getAddressLine2() 
+    {
+        return addressLine2;
+    }
+    public String getAddressLine3() 
+    {
+        return addressLine3;
+    }
+    public String getAddressPostcode() 
+    {
+        return addressPostcode;
+    }
+    public String getUniqueIdentifier() 
+    {
+        return uniqueIdentifier;
+    }
+    public String getDOB() 
+    {
+        return DOB;
+    }
+    public int getAge() 
+    {
+        return age;
+    }
+    public EPersonType getPersonType() 
+    {
+        return personType;
     }
 
+    
     
 }
