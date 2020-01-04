c038cebcf0010f6557ba319bab7f06322fd1676c
Alex Martin <example@test.com>
2020-01-04 21:27:34 +0000

    Class ViewMedicineList created in viewData package

---
+++ JavaProject/Soft252AMartin/src/viewData/ViewMedicineList.java
@@ -0,0 +1,17 @@
+package viewData;
+
+import static viewData.ViewData.getMedicineList;
+
+public class ViewMedicineList 
+{
+    /**
+     * Method to get a list of all medicines and their details.
+     * If there are no medicines then an array of one element is returned containing "NO MEDICINES"
+     * @return String[]
+     */
+    public String[] getMedicinesList()
+    {
+        String path = "res\\Medication\\medicine.csv";
+        return getMedicineList(path);
+    }
+}
