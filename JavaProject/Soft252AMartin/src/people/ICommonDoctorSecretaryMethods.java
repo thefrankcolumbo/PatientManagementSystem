7eb4c96d41a55603947af4b3f5913d329bec086e
Alex Martin <example@test.com>
2020-01-05 15:57:23 +0000

    Interface commonDoctorSecretaryMethods updated with a method to get a calender for a spefic doctor

--- JavaProject/Soft252AMartin/src/people/ICommonDoctorSecretaryMethods.java
+++ JavaProject/Soft252AMartin/src/people/ICommonDoctorSecretaryMethods.java
@@ -6,4 +6,5 @@ public interface ICommonDoctorSecretaryMethods
 {
     public void createAppointment();
     public String[] getMedicineList();
+    public String[] getCalenderForSpeficDoctor(String userID);
 }
